1ef6c8cd5ea1cf75142ea0f28f70901093bc9251
yyynnn <yyynnn@yyynnns-iMac.local>
2017-12-02 20:54:07 +0300

    deploy to heroku

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,26 @@
-process.env.NODE_ENV = 'production';
-var path = require('path');
-var webpack = require('webpack');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+process.env.NODE_ENV = "production";
+var path = require("path");
+var webpack = require("webpack");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: [path.join(__dirname, '/client/index.js')],
+  entry: [path.join(__dirname, "/client/index.js")],
   output: {
-    path: path.join(__dirname, 'public'),
-    filename: '[name].js',
-    libraryTarget: 'umd',
-    publicPath: ''
+    path: path.join(__dirname, "public"),
+    filename: "[name].js",
+    libraryTarget: "umd",
+    publicPath: ""
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Maillord',
-      favicon: path.join(__dirname, 'favicon.ico'),
-      template: path.join(__dirname, 'templates/index-template.html')
+      title: "Offerlord",
+      favicon: path.join(__dirname, "favicon.ico"),
+      template: path.join(__dirname, "templates/index-template.html")
     }),
     new webpack.DefinePlugin({
-      'process.env': {
-        NODE_ENV: JSON.stringify('production')
+      "process.env": {
+        NODE_ENV: JSON.stringify("production")
       }
     }),
     new webpack.optimize.UglifyJsPlugin({
@@ -29,21 +29,29 @@ module.exports = {
         warnings: false
       }
     }),
-    new ExtractTextPlugin('bundle.css')
+    new ExtractTextPlugin("bundle.css")
   ],
   module: {
     rules: [
       {
         test: /\.(gif|png|jpe?g|svg)$/i,
         loaders: [
-          'file-loader',
+          "file-loader",
           {
-            loader: 'image-webpack-loader',
+            loader: "image-webpack-loader",
             query: {
-              progressive: true,
+              mozjpeg: {
+                progressive: true
+              },
+              gifsicle: {
+                interlaced: false
+              },
+              optipng: {
+                optimizationLevel: 4
+              },
               pngquant: {
-                quality: '65-90',
-                speed: 4
+                quality: "75-90",
+                speed: 3
               }
             }
           }
@@ -54,25 +62,25 @@ module.exports = {
         loader: ExtractTextPlugin.extract({
           use: [
             {
-              loader: 'css-loader?importLoaders=1!postcss-loader'
+              loader: "css-loader?importLoaders=1!postcss-loader"
             }
           ],
-          fallback: 'style-loader'
+          fallback: "style-loader"
         })
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2)$/,
-        loader: 'url-loader?limit=1024&name=fonts/[name].[ext]'
+        loader: "url-loader?limit=1024&name=fonts/[name].[ext]"
       },
       {
         test: /\.js$/,
-        loaders: ['babel-loader'],
-        include: path.join(__dirname, 'client'),
-        exclude: path.join(__dirname, 'node_modules')
+        loaders: ["babel-loader"],
+        include: path.join(__dirname, "client"),
+        exclude: path.join(__dirname, "node_modules")
       }
     ]
   },
   resolve: {
-    extensions: ['*', '.js', '.jsx']
+    extensions: ["*", ".js", ".jsx"]
   }
 };
