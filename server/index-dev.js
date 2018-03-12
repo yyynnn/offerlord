71f7265be21b68358211c66f9056838abf77c0e8
Your Name <you@example.com>
2018-03-12 22:01:24 +0300

    ssl fix

--- server/index-dev.js
+++ server/index-dev.js
@@ -1,12 +1,12 @@
-import express from "express";
-import path from "path";
-import fs from "fs";
-import webpack from "webpack";
-import webpackMiddleware from "webpack-dev-middleware";
-import webpackHotMiddleware from "webpack-hot-middleware";
-import webpackConfig from "../webpack.config.dev";
-import bodyParser from "body-parser";
-import Handlebars from "handlebars";
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev';
+import bodyParser from 'body-parser';
+import Handlebars from 'handlebars';
 
 var requestSejda = require('request');
 
@@ -16,25 +16,33 @@ const compiler = webpack(webpackConfig);
 const router = express.Router();
 
 app.use(bodyParser.json());
-app.post("/downloaddata", update);
+app.post('/downloaddata', update);
 
 function update(req, res) {
-  let source = fs.readFile("./templates/offerTemplate.html", "utf8", function(
+  var source = fs.readFile('./templates/offerTemplate.html', 'utf8', function(
     err,
     data
   ) {
     if (err) throw err;
-    let dataFront = data.toString();
-    let compile = Handlebars.compile(dataFront);
-    let result = compile(req.body);
+    var dataFront = data.toString();
+    var compile = Handlebars.compile(dataFront);
+    var result = compile(req.body);
     var compiledString = result.toString();
-    var options = { method: 'POST', encoding: 'binary', json: { type: 'htmlToPdf', htmlCode: compiledString }, uri: 'https://api.sejda.com/v1/tasks', headers: { 'Content-Type': 'application/json' } };
+    var options = {
+      method: 'POST',
+      rejectUnauthorized: false,
+      encoding: 'binary',
+      json: { type: 'htmlToPdf', htmlCode: compiledString },
+      uri: 'https://api.sejda.com/v1/tasks',
+      headers: { 'Content-Type': 'application/json' }
+    };
     var callback = function(error, response, body) {
-			fs.writeFile(`./download/offer.pdf`, body, 'binary', function(err) {
-				if (err) throw err;
-				res.sendStatus(200);
-			});
-		};
+      fs.writeFile(`./download/offer.pdf`, body, 'binary', function(err) {
+        if (err) throw err;
+        console.log(error);
+        res.sendStatus(200);
+      });
+    };
     requestSejda(options, callback);
   });
 }
@@ -47,15 +55,14 @@ app.use(
 );
 app.use(webpackHotMiddleware(compiler));
 
-router.get("/download", function(req, res) {
+router.get('/download', function(req, res) {
   let file = path.join(__dirname, `../download/offer.pdf`);
   res.download(file, file);
 });
-app.use("/", router);
+app.use('/', router);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "./index-dev.html"));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './index-dev.html'));
 });
 
-app.listen(port, () => console.log("Running on localhost:" + port));
-
+app.listen(port, () => console.log('Running on localhost:' + port));
--- server/index-production.js
+++ server/index-production.js
@@ -1,11 +1,11 @@
-"use strict";
+'use strict';
 
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-var bodyParser = require("body-parser");
-var Handlebars = require("handlebars");
-var serveStatic = require("serve-static");
+var express = require('express');
+var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+var Handlebars = require('handlebars');
+var serveStatic = require('serve-static');
 var requestSejda = require('request');
 
 var app = express();
@@ -13,46 +13,47 @@ var port = process.env.PORT || 8080;
 var router = express.Router();
 
 app.use(bodyParser.json());
-app.post("/downloaddata", update);
+app.post('/downloaddata', update);
 
 function update(req, res) {
-	var source = fs.readFile('./templates/offerTemplate.html', 'utf8', function(
-		err,
-		data
-	) {
-		if (err) throw err;
-		var dataFront = data.toString();
-		var compile = Handlebars.compile(dataFront);
-		var result = compile(req.body);
-		var compiledString = result.toString();
-		var options = {
-			method: 'POST',
-			encoding: 'binary',
-			json: { type: 'htmlToPdf', htmlCode: compiledString },
-			uri: 'https://api.sejda.com/v1/tasks',
-			headers: { 'Content-Type': 'application/json' }
-		};
-		var callback = function(error, response, body) {
-			fs.writeFile(`./download/offer.pdf`, body, 'binary', function(err) {
-				if (err) throw err;
-				res.sendStatus(200);
-			});
-		};
-		requestSejda(options, callback);
-	});
+  var source = fs.readFile('./templates/offerTemplate.html', 'utf8', function(
+    err,
+    data
+  ) {
+    if (err) throw err;
+    var dataFront = data.toString();
+    var compile = Handlebars.compile(dataFront);
+    var result = compile(req.body);
+    var compiledString = result.toString();
+    var options = {
+      method: 'POST',
+      encoding: 'binary',
+      rejectUnauthorized: false,
+      json: { type: 'htmlToPdf', htmlCode: compiledString },
+      uri: 'https://api.sejda.com/v1/tasks',
+      headers: { 'Content-Type': 'application/json' }
+    };
+    var callback = function(error, response, body) {
+      fs.writeFile(`./download/offer.pdf`, body, 'binary', function(err) {
+        if (err) throw err;
+        res.sendStatus(200);
+      });
+    };
+    requestSejda(options, callback);
+  });
 }
 
-app.use(serveStatic(path.join(__dirname, "../download/")));
-router.get("/download", function(req, res) {
+app.use(serveStatic(path.join(__dirname, '../download/')));
+router.get('/download', function(req, res) {
   var file = path.join(__dirname, `../download/offer.pdf`);
   res.download(file, file);
 });
 
-app.use("/", router);
-app.use(serveStatic(path.join(__dirname, "../public/")));
+app.use('/', router);
+app.use(serveStatic(path.join(__dirname, '../public/')));
 
-app.get("/", function(req, res) {
-  res.sendFile(path.join(__dirname, "../public/index.html"));
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.listen(port);
